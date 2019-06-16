72f6c6c87374c72cabf4f783fa7d5b6dd2b75b17
Sören Schröder <code@razupaltuff.eu>
2019-06-16 22:33:37 +0200

    Improve contact card design

--- src/components/molecules/Contact.vue
+++ src/components/molecules/Contact.vue
@@ -7,6 +7,7 @@
     padding: 5px;
     flex: 0 0 300px;
     color: $white;
+    flex-basis: calc(100% - 10px);
 
     .portrait {
       width: 85px;
@@ -19,13 +20,16 @@
         font-size: 0.7rem;
         margin: 15px 0 0 0;
       }
+
       .name {
         font-size: 1rem;
         margin: 8px 0 0 0;
       }
+
       .email {
-        font-size: 0.49rem;
+        font-size: 0.6rem;
         margin: 5px 0 0 0;
+
         a {
           color: inherit;
           text-decoration: none;
