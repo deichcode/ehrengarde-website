5134b7c7237ab6ba3ba3e7e8d8169542cbc91395
Sören Schröder <code@razupaltuff.eu>
2019-08-26 17:15:59 +0200

    Serve static files from API

--- Backend/Ehrengarde.Api/Startup.cs
+++ Backend/Ehrengarde.Api/Startup.cs
@@ -45,6 +45,7 @@ namespace Ehrengarde.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
             if (env.IsDevelopment())
             {
                 app.UseCors(AllowAnyOrigin);
