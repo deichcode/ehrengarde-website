59a73dd2c5e4513e9ca396bf59321141f349d72d
Sören Schröder <code@razupaltuff.eu>
2019-08-28 18:56:48 +0200

    Create rudimentray imprint

--- Frontend/src/views/Impressum.vue
+++ Frontend/src/views/Impressum.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-  <p>Impressum</p>
+    <p>Impressum</p>
+    <p>
+      Ehrengarde der Stadt Neuwied e.V.
+      <br/>
+      info@ehrengarde-neuwied.de
+      <br/>
+      Markus Schröder
+      <br/>
+      Hochstraße 128
+      <br/>
+      56566 Neuwied
+      <br/>
+      (0 26 31) 49 81 39
+      <br/>
+      Eingetragen beim Amtsgericht
+      unter Vereinsregister Nr. 11338
+    </p>
   </div>
 </template>
 
