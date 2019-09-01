773454538e5931e6721e674d25a75f0d8f13738b
Sören Schröder <code@razupaltuff.eu>
2019-09-01 12:01:51 +0200

    Add Datenschutz page

---
+++ Frontend/src/views/Datenschutz.vue
@@ -0,0 +1,33 @@
+<style scoped lang="scss">
+  .card {
+    max-width: 600px;
+  }
+
+  .wrapper {
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
+
+<template>
+  <div class="content">
+    <h2 class="title">Datenschutz</h2>
+    <div class="wrapper">
+      <Card class="card">
+        <p>
+          Platzhalter für Datenschutzerklärung
+        </p>
+      </Card>
+    </div>
+  </div>
+
+</template>
+
+<script>
+import Card from '../components/atoms/Card.vue';
+
+export default {
+  name: 'Datenschutz',
+  components: { Card },
+};
+</script>
