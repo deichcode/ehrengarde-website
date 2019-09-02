b66726c5189b7edb398d1f0f96ada7cf4e0f0d77
Bjarne <bjarne98@gmx.com>
2019-09-02 21:55:37 +0200

    Add Seperator to project

---
+++ Frontend/src/components/atoms/Separator.vue
@@ -0,0 +1,22 @@
+<style scoped lang="css">
+
+.separator{
+    text-align: center;
+    height: 0px;
+    font-size: 2.0rem;
+    margin-bottom: 50px;
+    margin-top: -30px;
+}
+</style>
+
+<template>
+    <div class="separator">
+        <p>...................</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Separator',
+};
+</script>
